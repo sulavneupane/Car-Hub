7bb5162f6f3dda986e957ad60c7a0f2a0bb8de2f
Sulav Neupane <sulavneupane@gmail.com>
2024-06-11 22:24:14 -0400

    initial commit for client side codes

---
+++ README.md
@@ -0,0 +1,13 @@
+# Car Showcase
+This is a simple project that displays the information about different cars. The cars are retrieved using Cars by API-Ninjas library from RapidAPI.
+
+You can view the working project at: URL
+
+## Tech Stack:
+1. React: https://react.dev/
+2. NextJS: https://nextjs.org/
+3. TailwindCSS: https://tailwindcss.com/
+4. Headless UI: https://headlessui.com/
+5. Rapid API: https://rapidapi.com/
+6. Cars by API-Ninjas: https://rapidapi.com/apininjas/api/cars-by-api-ninjas
+7. Imagin Studio: https://www.imaginstudio.com/
---
+++ components/SearchBar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, {useState} from "react";
+import {useRouter} from "next/navigation";
+
+import SearchManufacturer from "./SearchManufacturer";
+import {SearchButton} from "@/components/index";
+import Image from "next/image";
+
+function SearchBar({setManufacturer, setModel}) {
+    const [searchManufacturer, setSearchManufacturer] = useState('');
+    const [searchModel, setSearchModel] = useState('');
+    const router = useRouter();
+
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (searchManufacturer === '' && searchModel === '') {
+            return alert('Please fill in the search bar');
+        }
+
+        setModel(searchModel);
+        setManufacturer(searchManufacturer);
+    }
+
+    return (
+        <form className={"searchbar"} onSubmit={handleSearch}>
+            <div className={"searchbar__item"}>
+                <SearchManufacturer
+                    selected={searchManufacturer}
+                    setSelected={setSearchManufacturer}
+                />
+
+                <SearchButton otherClasses={"sm:hidden"}/>
+            </div>
+
+            <div className={"searchbar__item"}>
+                <Image
+                    src={"/model-icon.png"}
+                    width={25}
+                    height={25}
+                    className={"absolute w-[20px] h-[20px] ml-4"}
+                    alt={"car model"}
+                />
+                <input
+                    type={"text"}
+                    name={"model"}
+                    value={searchModel}
+                    onChange={(e) => setSearchModel(e.target.value)}
+                    placeholder={"Tiguan"}
+                    className={"searchbar__input"}
+                />
+                <SearchButton otherClasses={"sm:hidden"}/>
+            </div>
+            <SearchButton otherClasses={"max-sm:hidden"}/>
+        </form>
+    );
+}
+
+export default SearchBar;
