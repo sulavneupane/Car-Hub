265b91a29512ecbdcdbf8a81228112ba832fb96a
Sulav Neupane <sulavneupane@gmail.com>
2024-06-11 22:49:10 -0400

    chore: few cleanups

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 import CustomButton from './CustomButton';
 
-function Navbar(props) {
+function Navbar() {
     return (
         <header className={"w-full absolute z-10"}>
             <nav className={"max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4"}>
